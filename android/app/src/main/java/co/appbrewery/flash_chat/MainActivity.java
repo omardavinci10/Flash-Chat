3749b08470bee40db3e6e1952ea5dfd6daefda94
Omar Mohamed <75180396+omardavinci10@users.noreply.github.com>
2022-11-17 16:21:42 +0200

    Removes all .gitignore files and folders

--- android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
@@ -1,5 +1,5 @@
 package co.appbrewery.flash_chat;
 
-+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {}
